c05744ccc11931a3184ac75dbdf5fcc26d9ac345
Senatorgbolahan <efunkoyaomogbolahan@gmail.com>
2022-03-01 15:47:22 +0100

    2 files modified

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './style.css';
 import SearchText from './components/SearchText';
 
 export default function App() {
+  const usersData = [
+    { id: 1, name: 'Tania', username: 'floppydiskette' },
+    { id: 2, name: 'Craig', username: 'siliconeidolon' },
+    { id: 3, name: 'Ben', username: 'benisphere' },
+  ];
+
   return (
     <section>
       <SearchText />
--- src/components/SearchText.jsx
+++ src/components/SearchText.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const SearchText = () => {
-  const usersData = [
-    { id: 1, name: 'Tania', username: 'floppydiskette' },
-    { id: 2, name: 'Craig', username: 'siliconeidolon' },
-    { id: 3, name: 'Ben', username: 'benisphere' },
-  ];
+  // const usersData = [
+  //   { id: 1, name: 'Tania', username: 'floppydiskette' },
+  //   { id: 2, name: 'Craig', username: 'siliconeidolon' },
+  //   { id: 3, name: 'Ben', username: 'benisphere' },
+  // ];
 
   return <section>{console.log('Hello')}</section>;
 };
