170a01b537008c26370a767807ba8d794f2072f3
Senatorgbolahan <efunkoyaomogbolahan@gmail.com>
2022-03-01 17:26:50 +0100

    1 file modified

--- src/components/SearchText.jsx
+++ src/components/SearchText.jsx
@@ -15,6 +15,7 @@ const SearchText = ({ searchWord, handleChange, handleMM }) => {
           onChange={handleChange}
           value={searchWord}
           placeholder="Search"
+          oninput={handleChange}
         />
       </form>
 
