d833ca0aee9a13332c4037aa0945107f2d2580f3
Senatorgbolahan <efunkoyaomogbolahan@gmail.com>
2022-03-01 15:45:21 +0100

    1 file modified

--- src/components/SearchText.jsx
+++ src/components/SearchText.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 const SearchText = () => {
+  const usersData = [
+    { id: 1, name: 'Tania', username: 'floppydiskette' },
+    { id: 2, name: 'Craig', username: 'siliconeidolon' },
+    { id: 3, name: 'Ben', username: 'benisphere' },
+  ];
+
   return <section>console.log('Hello');</section>;
 };
 
